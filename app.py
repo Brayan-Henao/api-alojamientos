34f6964b14d13d4cade30bb8b245d8aac0bd1e76
Brayan-Henao <brayanshenaoq@gmail.com>
2026-04-28 20:57:35 -0500

    chore: agregar herramientas de calidad de codigo

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from app import crear_app
 # Creamos la aplicacion
 app = crear_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Modo debug para reinicio automatico al guardar cambios
     app.run(debug=True, port=5000)
